7872e0883a7fbcee5e3418514179632c346d44df
Aurelien Allard <allard.au@gmail.com>
2018-05-24 21:36:05 +0200

    First

--- wonderbox/insolite_et_gourmand.py
+++ wonderbox/insolite_et_gourmand.py
@@ -1,43 +1,47 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import parsel
+import re
 
 
 class InsoliteEtGourmandSpider(scrapy.Spider):
     name = 'insolite-et-gourmand'
-    allowed_domains = ['https://www.wonderbox.fr']
+    allowed_domains = ['wonderbox.fr']
     start_urls = ['https://www.wonderbox.fr/b/week-end-insolite-et-gourmand/']
 
     def parse(self, response):
         self.log('Scraping ' + response.url)
-        for activity in response.css('div.activity-item'):
+        for activity in response.css('.activity-item'):
+
+          act = activity.css('.activity-title::text').extract_first()
+          loc = activity.css('.activity-title::text').extract_first().split()
+          zip_code = activity.xpath('.//h3[@class="activity-title"]/text()').re('[0-9]{2}')
+          rating = activity.xpath('.//span[@class="count"]/text()').re('[0-9]+')
+
+          if zip_code:
+            zc = activity.xpath('.//h3[@class="activity-title"]/text()').re('[0-9]{2}')[0]
+          else:
+            zc = 'n.a.'
+
+          if rating:
+            rat = len(activity.css('span.full').extract())
+            ratc = activity.xpath('.//span[@class="count"]/text()').re('[0-9]+')[0]
+          else:
+            rat = 'n.a.'
+            ratc = '0'
+
           items = {
-            'activity-title': activity.css('h3.activity-title::text').extract(),
+            'activity-title': act,
+            'location': loc[len(loc)-2],
+            'ZIP': zc,
+            'rating': rat,
+            'rating-count': ratc,
           }
+
           yield items
 
-          next_page_url = response.css('div.pagination > a.js-pagination-next::attr(href)').extract()
-          if next_page_url:
-            next_page_url = next_page_url[0].rsplit('/', 1)[1]
-            next_page_url = response.urljoin(next_page_url)
-            yield scrapy.Request(url = next_page_url, callback = self.parse)
+        next_page_url = response.css('div.pagination > a.js-pagination-next::attr(href)').extract_first()
 
-
-# class QuotesSpider(scrapy.Spider):
-#     name = 'quotes'
-#     allowed_domains = ['toscrape.com']
-#     start_urls = ['http://quotes.toscrape.com']
-
-#     def parse(self, response):
-#         self.log('Scraping ' + response.url)
-#         for quote in response.css('div.quote'):
-#           item = {
-#             'author_name': quote.css('small.author::text').extract_first(),
-#             'text': quote.css('span.text::text').extract_first(),
-#             'tags': quote.css('a.tag::text').extract(),
-#           }
-#           yield item
-
-#         if next_page_url:
-#           next_page_url = reponse.css('li.next > a::attr(href)').extract_first()
-#           next_page_url = response.urljoin(next_page_url)
-#           yield scrapy.Request(url = next_page_url, callback = self.parse)
+        if next_page_url:
+          next_page_url = response.urljoin(next_page_url)
+          yield scrapy.Request(url = next_page_url, callback = self.parse)
